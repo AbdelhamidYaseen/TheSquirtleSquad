c8391f998182cc77f22e9beadd918f52517811f3
Peach <114141082+Vanroostyaseen@users.noreply.github.com>
2023-05-16 12:19:23 +0200

    asides fixed desktop version

--- controllers/homeController.ts
+++ controllers/homeController.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import { getUserById, iUser } from '../models/usersModel';
 
 const controller = {
-    get: (req: express.Request, res : express.Response) => {
-        res.render('home');
+    get: async (req: express.Request, res : express.Response) => {
+        try {
+            // Log the names of all the users in the array
+            const user : iUser = await getUserById(1);
+            res.render('home', {user:user});
+        } catch (err : any) {
+            console.error(err.message);
+            res.status(500).send('Internal Server Error');
+        }
     },
     post: (req: express.Request, res: express.Response) => {
         test();
