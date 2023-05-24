278630f0d12a550ab57e09a8f440a2b4c29dc1bb
Peach <114141082+Vanroostyaseen@users.noreply.github.com>
2023-05-24 15:10:40 +0200

    templates zijn gefixt

--- controllers/indexController.ts
+++ controllers/indexController.ts
@@ -6,13 +6,8 @@ import { getUserById, iUser } from '../models/usersModel';
 const controller = {
     get: async (req: express.Request, res : express.Response) => {
         const user : iUser = await getUserById(1);
-        const pokemonNumber : number = Math.floor(Math.random() * 150);
-        const apiFetch : iPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`).then((response)=> response.json());
 
-        const getBuddy = await getBuddyFromUser(1);
-        const apiFetchBuddy : iPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${getBuddy?.pokemon_id}`).then((response) => response.json());
-
-        res.render('index',{buddy:apiFetchBuddy,user:user, pokemon : apiFetch, getBuddyFromUser});
+        res.render('index',{ getBuddyFromUser});
     },
     post: (req: express.Request, res: express.Response) => {
         test();
